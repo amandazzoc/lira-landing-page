b8116f00794b9227ab2546516d5ce7a117bdb3eb
Amanda Oliveira <amandaol7b@gmail.com>
2024-11-09 19:51:52 -0300

    :sparkles: feat: Adição do footer e finalização da sessão equipe

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -19,31 +19,3 @@ navLinks.forEach((link) => {
   });
 });
 
-// Garantir que ao clicar no link vá parar antes do conteudo, dessa forma a nav não vai tampar
-document.addEventListener("DOMContentLoaded", function () {
-  // Defina o offset desejado em pixels
-  var offset = 120; // ajuste este valor conforme necessário
-
-  // Capture todos os links da navbar
-  var navbarLinks = document.querySelectorAll('nav a[href^="#"]');
-
-  // Adicione um evento de clique a cada link
-  navbarLinks.forEach(function (link) {
-    link.addEventListener("click", function (event) {
-      event.preventDefault(); // Previna o comportamento padrão de ancoragem
-
-      // Obtenha o ID da seção alvo
-      var targetId = this.getAttribute("href").substring(1);
-      var targetElement = document.getElementById(targetId);
-
-      // Calcule a posição de destino com o offset
-      var targetPosition = targetElement.offsetTop - offset;
-
-      // Role suavemente até a posição de destino ajustada
-      window.scrollTo({
-        top: targetPosition,
-        behavior: "smooth",
-      });
-    });
-  });
-});
